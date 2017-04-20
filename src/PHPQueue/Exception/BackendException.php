357fb55404c93284973006c5c22f3e8b31616c07
Elliott Eggleston <ejegg@ejegg.com>
2017-04-19 17:11:03 -0700

    Blank deprecated repo
    Upstreamed our stuff, no need for this copy
    
    Change-Id: Ibd586ba572f6a40e48034cd611bf6f852d4cec56

--- src/PHPQueue/Exception/BackendException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace PHPQueue\Exception;
-
-/**
- * Exception thrown when job cannot be loaded nor found
- */
-class BackendException extends Exception
-{
-}
--- src/PHPQueue/Exception/JsonException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace PHPQueue\Exception;
-
-/**
- * Something bad with the JSON
- */
-class JsonException extends Exception
-{
-}
